77ff6aad32cd2f422965ed8855f9071122ff7462
Nancy Som <nancysom2712@gmail.com>
2026-03-16 12:06:12 +0530

    Link to Nav-bar added

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { LOGO_URL } from "../utils/constants";
+import {Link} from 'react-router-dom';
 
 const Header = ()=> {
 const [logBtn, setLogBtn] = useState("Login");
@@ -10,10 +11,10 @@ const [logBtn, setLogBtn] = useState("Login");
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                    <li>Cart</li>
+                   <li> <Link to='/'>Home</Link></li>
+                    <li> <Link to='/about'>About us</Link></li>
+                    <li> <Link to='/contact'>Contact us</Link></li>
+                    <li> <Link to='/link'>Cart</Link></li>
                     <button 
                     className="log-button"
                     onClick={()=>logBtn==="Login"? setLogBtn("Logout") : setLogBtn("Login")}>
